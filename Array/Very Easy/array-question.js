32e99da4e0326e55bc95d8f751aea72c53aa2c75
Mahir7878 <mahirmahir8951@gmail.com>
2024-05-18 15:21:36 +0530

    in this repo we solve daily random questions

--- Array/Very Easy/array-question.js
+++ Array/Very Easy/array-question.js
@@ -313,8 +313,191 @@ function parseArray(arr){
 }
 
 // Examples
-console.log(parseArray([1, 2, "a", "b"])) //➞ ["1", "2", "a", "b"]
-console.log(parseArray(["abc", 123, "def", 456])) //➞ ["abc", "123", "def", "456"]
-console.log(parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]))// ➞ ["1", "2", "3", "17", "24", "3", "a", "123b"]
-console.log(parseArray([])) //➞ []
+// console.log(parseArray([1, 2, "a", "b"])) //➞ ["1", "2", "a", "b"]
+// console.log(parseArray(["abc", 123, "def", 456])) //➞ ["abc", "123", "def", "456"]
+// console.log(parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]))// ➞ ["1", "2", "3", "17", "24", "3", "a", "123b"]
+// console.log(parseArray([])) //➞ []
 
+
+//Q=> 21 Create a function that takes an array and returns the types of values (data types) in a new array.
+
+function arrayValuesTypes(arr){
+    return arr.map((item)=> typeof item)
+}
+
+// // Examples
+// console.log(arrayValuesTypes([1, 2, "null", []]))
+// // ➞ ["number", "number", "string", "object"]
+// console.log(arrayValuesTypes(["214", true, false, 2, 2.15, [], null]))
+// // ➞ ["string", "boolean", "boolean", "number", "number", "object", "object"]
+// console.log(arrayValuesTypes([21.1, "float", "array", ["I am array"], null, true, 214]))
+// // ➞ ["number", "string", "string", "object", "object", "boolean", "number"]
+
+
+//Q => 22 There is an easy way to assign to array values to the nth index by using rest parameter syntax.
+
+// var [head, tail] = [1, 2, 3, 4]
+
+// console.log(head) // outputs  1
+// console.log(tail) // outputs 2
+// But how could I make tail = [2, 3, 4] instead of tail = 2?
+
+// solution
+var [head,...tail] = [1,2,3,4];
+// console.log(head);
+// console.log(tail);
+
+
+//Q => 23 Create a function that takes as a parameter an array of "stringified" numbers and returns an array of numbers.
+
+function toNumberArray(arr){
+    return arr.map((item)=> Number(item));
+}
+// Example:
+// console.log(toNumberArray(["9.4", "4.2"])) //➞ [9.4, 4.2]
+// console.log(toNumberArray(["91", "44"])) //➞ [91, 44]
+// console.log(toNumberArray(["9.5", "8.8"])) //➞ [9.5, 8.8]
+
+
+//Q => 24 Create a function that takes an array of two numbers and checks if the square root of the first number is equal to the cube root of the second number.
+
+function checkSquareAndCube(arr){
+    return Math.sqrt(arr[0]) === Math.pow(arr[1], 1/3)
+}
+
+// Examples
+// console.log(checkSquareAndCube([4, 8]))//➞ true
+// console.log(checkSquareAndCube([16, 48])) //➞ false
+// console.log(checkSquareAndCube([9, 27])) //➞ true
+
+
+//Q => 25 You call your spouse in anger and a "little" argument takes place. Count the total amount of insults used. Given an object of insults, return the total amount of insults used.
+
+function totalAmountAdjectives(obj){
+    return Object.keys(obj).length;
+}
+// Examples
+// console.log(totalAmountAdjectives({ a: "moron" }))// ➞ 1
+// console.log(totalAmountAdjectives({ a: "idiot", b: "idiot", c: "idiot" })) //➞ 3
+// console.log(totalAmountAdjectives({ a: "moron", b: "scumbag", c: "moron", d: "dirtbag" })) //➞ 4
+
+//Q=> 26 You can assign variables from arrays with destructuring like this:
+
+const arr1 = ["eyes", "nose", "lips", "ears"];
+// let [eyes, nose, lips, ears] = arr1
+// But you can also skip over items in the array being destructured.
+
+let [eyes, nose, , ears] = arr1;
+
+
+//Q=> 27 Create a function which returns the length of a string, WITHOUT using String.length property.
+
+function length(str){
+    let count = 0;
+    for(let length of str){
+        count++
+    }
+    return count
+}
+
+
+// Examples
+// console.log(length("Hello World")) //➞ 11
+// console.log(length("Edabit")) //➞ 6
+// console.log(length("wash your hands!")) //➞ 16
+
+//Q=>28 Create a function that takes a number and return an array of three numbers: half of the number, quarter of the number and an eighth of the number.
+
+function halfQuarterEighth(num){
+    return [num / 2, num / 4, num / 8]
+}
+
+// Examples
+// console.log(halfQuarterEighth(6))//➞ [3, 1.5, 0.75]
+// console.log(halfQuarterEighth(22)) //➞ [11, 5.5, 2.75]
+// console.log(halfQuarterEighth(25)) //➞ [12.5, 6.25, 3.125]
+
+//Q => 29 Create a function that takes a whole number as input and returns the shape with that number's amount of sides. Here are the expected outputs to get from these inputs.
+
+// Inputs	Outputs
+// 1	"circle"
+// 2	"semi-circle"
+// 3	"triangle"
+// 4	"square"
+// 5	"pentagon"
+// 6	"hexagon"
+// 7	"heptagon"
+// 8	"octagon"
+// 9	"nonagon"
+// 10	"decagon"
+
+function nSidedShape(n){
+    let shapes = ["circle","semi-circle","triangle","square","pentagon","hexagon","heptagon","heptagon","octagon","nonagon","decagon"];
+    return shapes[n - 1]
+}
+// Examples
+// console.log(nSidedShape(3)) // ➞ "triangle"
+// console.log(nSidedShape(1)) // ➞ "circle"
+// console.log(nSidedShape(9)) // ➞ "nonagon"
+
+//Q => 30 Create a function that takes an array with numbers and return an array with the elements multiplied by two.
+
+function getMultipliedArr(arr){
+    return arr.map(item => item * 2)
+}
+// // Examples
+// console.log(getMultipliedArr([2, 5, 3])) //➞ [4, 10, 6]
+// console.log(getMultipliedArr([1, 86, -5])) //➞ [2, 172, -10]
+// console.log(getMultipliedArr([5, 382, 0])) //➞ [10, 764, 0]
+
+
+//Q=> 31 Create a function that takes an array of words and transforms it into an array of each word's length.
+
+function wordLengths(arr){
+    return arr.map((str) => str.length)
+}
+
+// Examples
+console.log(wordLengths(["hello", "world"])) //➞ [5, 5]
+console.log(wordLengths(["Halloween", "Thanksgiving", "Christmas"])) //➞ [9, 12, 9]
+console.log(wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"])) //➞ [3, 5, 9, 4, 2, 3, 8]
+
+//Q => 32 Levers are simple machines with a rigid beam and a fulcrum. From the picture below, you can see that there are 3-types of levers: first class, second class and third class.
+
+// In a first class lever, the fulcrum is situated in the middle with the effort and the load being positioned opposite of each other.
+// In a second class lever, the fulcrum is situated in the right with the effort on the left and the load in the middle.
+// In a third class lever, the fulcrum is situated in the left with the effort being in the middle and the load being on the right.
+
+
+// Given an array that contains the fulcrum "f", the effort "e", and the load "l", write a function that determines whether or not the array shows a first class lever, second class lever, or a third class lever.
+
+function determineLever(arr){
+    const fulcrumIndex = arr.indexOf("f");
+    const effortIndex = arr.indexOf("e");
+    const loadIndex = arr.indexOf("l");
+
+    if(fulcrumIndex === 1){
+        return "first class lever"
+    }else if(fulcrumIndex === 2){
+        return "second class lever"
+    }else if(fulcrumIndex === 0){
+        return "third class lever"
+    }
+}
+
+// Examples
+console.log(determineLever(["e", "f", "l"])) // ➞ "first class lever"
+console.log(determineLever(["e", "l", "f"])) // ➞ "second class lever"
+console.log(determineLever(["f", "e", "l"])) // ➞ "third class lever"
+
+//Q => 33 Create a function that takes a string and returns a string with spaces in between all of the characters.
+
+function spaceMeOut(str){
+    let charactersArray = str.split('')
+    let spaceString = charactersArray.join(' ')
+    return spaceString;
+}
+// Examples
+console.log(spaceMeOut("space")) // ➞ "s p a c e"
+console.log(spaceMeOut("far out")) // ➞ "f a r   o u t"
+console.log(spaceMeOut("elongated musk")) // ➞ "e l o n g a t e d   m u s k"
