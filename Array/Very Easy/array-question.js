dc703adcd477be5f27a6b9c12ab7387bddb61026
Mahir7878 <mahirmahir8951@gmail.com>
2024-05-19 14:50:21 +0530

    In this repo we solve some random question daily

--- Array/Very Easy/array-question.js
+++ Array/Very Easy/array-question.js
@@ -458,9 +458,9 @@ function wordLengths(arr){
 }
 
 // Examples
-console.log(wordLengths(["hello", "world"])) //➞ [5, 5]
-console.log(wordLengths(["Halloween", "Thanksgiving", "Christmas"])) //➞ [9, 12, 9]
-console.log(wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"])) //➞ [3, 5, 9, 4, 2, 3, 8]
+// console.log(wordLengths(["hello", "world"])) //➞ [5, 5]
+// console.log(wordLengths(["Halloween", "Thanksgiving", "Christmas"])) //➞ [9, 12, 9]
+// console.log(wordLengths(["She", "sells", "seashells", "down", "by", "the", "seashore"])) //➞ [3, 5, 9, 4, 2, 3, 8]
 
 //Q => 32 Levers are simple machines with a rigid beam and a fulcrum. From the picture below, you can see that there are 3-types of levers: first class, second class and third class.
 
@@ -486,9 +486,9 @@ function determineLever(arr){
 }
 
 // Examples
-console.log(determineLever(["e", "f", "l"])) // ➞ "first class lever"
-console.log(determineLever(["e", "l", "f"])) // ➞ "second class lever"
-console.log(determineLever(["f", "e", "l"])) // ➞ "third class lever"
+// console.log(determineLever(["e", "f", "l"])) // ➞ "first class lever"
+// console.log(determineLever(["e", "l", "f"])) // ➞ "second class lever"
+// console.log(determineLever(["f", "e", "l"])) // ➞ "third class lever"
 
 //Q => 33 Create a function that takes a string and returns a string with spaces in between all of the characters.
 
@@ -498,6 +498,179 @@ function spaceMeOut(str){
     return spaceString;
 }
 // Examples
-console.log(spaceMeOut("space")) // ➞ "s p a c e"
-console.log(spaceMeOut("far out")) // ➞ "f a r   o u t"
-console.log(spaceMeOut("elongated musk")) // ➞ "e l o n g a t e d   m u s k"
+// console.log(spaceMeOut("space")) // ➞ "s p a c e"
+// console.log(spaceMeOut("far out")) // ➞ "f a r   o u t"
+// console.log(spaceMeOut("elongated musk")) // ➞ "e l o n g a t e d   m u s k"
+
+
+//Q=> 34 You can assign variables from nested arrays like this:
+
+// const arr = ["cars", "planes", ["trains", ["motorcycles"]]]
+
+// let trans1 = arr[0]
+// let trans2 = arr[1]
+// let trans3 = arr[2][0]
+// let trans4 = arr[2][1][0]
+
+// console.log(trans1) // outputs "cars"
+// console.log(trans2) // outputs "planes"
+// console.log(trans3) // outputs "trains"
+// console.log(trans4) // outputs "motorcycles"
+// With ES6, you can assign variables from arrays in a much more succinct way.
+
+const arr2 = ["cars", "planes", ["trains", ["motorcycles"]]];
+
+// Destructuring assignment
+const [trans1, trans2, [trans3, [trans4]]] = arr2;
+// console.log(trans1) // outputs "cars"
+// console.log(trans2) // outputs "planes"
+// console.log(trans3) // outputs "trains"
+// console.log(trans4) // outputs "motorcycles"
+
+
+//Q=>35 Create a program that will take two arrays of integers, a and b. Each array will consist of 3 positive integers, representing the dimensions of cuboids a and b. Find the difference of the cuboids' volumes.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+function findDifference(a,b){
+    let volumeOfA = a[0] * a[1] * a[2];
+    let volumeOfB = b[0] * b[1] * b[2];
+
+    return Math.abs(volumeOfA - volumeOfB);
+}
+
+// Examples
+// console.log(findDifference([ 28, 16, 29 ], [ 7, 8, 17 ]) ) //➞ 12040
+// console.log(findDifference([ 9, 22, 18 ], [ 16, 24, 10 ])) // ➞ 276
+// console.log(findDifference([ 1, 9, 25 ], [ 10, 7, 9 ]) )//➞  405
+// console.log(findDifference([ 7, 6, 16 ], [ 26, 9, 26 ]))// ➞  5412
+
+
+//Q=>36 Create a function that takes an array and returns the sum of all numbers in the array.
+
+function getSumOfItems(arr){
+    return arr.reduce((acc,curr) => acc + curr )
+}
+
+// Examples
+// console.log(getSumOfItems([2, 7, 4])) //➞ 13
+// console.log(getSumOfItems([45, 3, 0])) //➞ 48
+// console.log(getSumOfItems([-2, 84, 23]))// ➞ 105
+
+
+//Q=>37 Given an array of integers, determine whether the sum of its elements is even or odd.
+// The return value should be a string ("odd" or "even").
+// If the input array is empty, consider it as an array with a zero ([0]).
+
+
+// Examples
+// console.log(evenOrOdd([0])) // ➞ "even"
+// console.log(evenOrOdd([1])) // ➞ "odd"
+// console.log(evenOrOdd([]) ) //➞ "even"
+// console.log(evenOrOdd([0, 1, 5])) // ➞ "even"
+
+function evenOrOdd(arr){
+    let sum = 0;
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    // let sum = arr.reduce((acc,curr) => acc + curr);
+    return sum % 2 === 0 ? "even" : "odd";
+}
+
+//Q => 38 Create a function that takes an array of numbers arr and returns an inverted array.
+
+function invertArray(arr){
+    return arr.map(num => -num)
+}
+// Example
+console.log(invertArray([1, 2, 3, 4, 5])) //➞ [-1, -2, -3, -4, -5]
+console.log(invertArray([1, -2, 3, -4, 5])) // ➞ [-1, 2, -3, 4, -5]
+console.log(invertArray([])) // ➞ []
+
+
+//Q=> 39 Given an array, rotate the values clockwise by one (the last value is sent to the first position).
+// Check the examples for a better understanding.
+
+function rotateByOne(arr){  
+    const lastElement = arr.pop();
+    arr.unshift(lastElement);
+    return arr
+}
+
+// Examples
+// console.log(rotateByOne([1, 2, 3, 4, 5]))   //➞ [5, 1, 2, 3, 4]
+// console.log(rotateByOne([6, 5, 8, 9, 7]) )  //➞ [7, 6, 5, 8, 9]
+// console.log(rotateByOne([20, 15, 26, 8, 4]))// ➞ [4, 20, 15, 26, 8]
+
+
+//Q=>40 Create a function that returns the selected filename from a path. Include the extension in your answer.
+
+function getFilename(path){
+    const parts = path.split(/[/\\]/);
+    return parts[parts.length - 1];
+}
+
+// // Examples
+// console.log(getFilename("C:/Projects/pil_tests/ascii/edabit.txt"))// ➞ "edabit.txt"
+// console.log(getFilename("C:/Users/johnsmith/Music/Beethoven_5.mp3"))// ➞ "Beethoven_5.mp3"
+// console.log(getFilename("ffprobe.exe"))// ➞ "ffprobe.exe"
+
+
+//Q => 41 Write a function that takes an array of drinks and returns an array of only drinks with no sugar in them. Drinks that contain sugar (in this challenge) are:
+// cola
+// fanta
+
+function skipTooMuchSugarDrinks(arr){
+    return arr.filter((item)=>{
+        if(item !== "cola" && item !== "fanta"){
+            return item
+        }
+    })
+}
+// Examples
+// console.log(skipTooMuchSugarDrinks(["fanta", "cola", "water"]))// ➞ ["water"]
+// console.log(skipTooMuchSugarDrinks(["fanta", "cola"])) //➞ []
+// console.log(skipTooMuchSugarDrinks(["lemonade", "beer", "water"])) //➞ ["lemonade", "beer", "water"]
+
+
+//Q => 42 Given an array of numbers, create a function which returns the same array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
+
+function addIndexes(arr){
+    
+    return arr.map((item,index)=> item + index);
+}
+
+// Examples
+// console.log(addIndexes([0, 0, 0, 0, 0])) // ➞ [0, 1, 2, 3, 4]
+// console.log(addIndexes([1, 2, 3, 4, 5])) // ➞ [1, 3, 5, 7, 9]
+// console.log(addIndexes([5, 4, 3, 2, 1])) // ➞ [5, 5, 5, 5, 5]
+
+
+//Q => 43 Create a function that returns an array of all the integers between two given numbers start and end.
+
+function rangeOfNum(start,end){
+    let arr = [];
+    for(let i = start; i< end; i++){
+        arr.push(i)
+    }
+    return arr
+}
+
+// Examples
+console.log(rangeOfNum(2, 4) ) // ➞ [3]
+console.log(rangeOfNum(5, 9) ) // ➞ [6, 7, 8]
+console.log(rangeOfNum(2, 11)) // ➞ [3, 4, 5, 6, 7, 8, 9, 10]
+
+
+//Q=>44 Write a function that returns the sum of elements greater than 5, in the given array .
+
+function sumFive(arr){
+    return arr
+    .filter(item => item > 5)
+    .reduce((acc,curr) => acc + curr,0)
+}
+// Examples
+console.log(sumFive([1, 5, 20, 30, 4, 9, 18]))// ➞ 77
+console.log(sumFive([1, 2, 3, 4]))// ➞ 0
+console.log(sumFive([10, 12, 28, 47, 55, 100]))// ➞ 252
