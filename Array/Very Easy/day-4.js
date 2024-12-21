700ab4c0d278cb8c206efb9946abba88f2e6ba83
Mahir7878 <mahirmahir8951@gmail.com>
2024-12-21 23:54:47 +0530

    Edabit Question

--- Array/Very Easy/day-4.js
+++ Array/Very Easy/day-4.js
@@ -1,3 +1,4 @@
+
 //Q => 42 Given an array of numbers, create a function which returns the same array but with each element's index in the array added to itself. This means you add 0 to the number at index 0, add 1 to the number at index 1, etc...
 
 function addIndexes(arr) {
@@ -10,6 +11,7 @@ function addIndexes(arr){
     for(let i = 0; i < arr.length ; i++){
         res.push(arr[i] + i)
     }
+    
     return res
 }
 
